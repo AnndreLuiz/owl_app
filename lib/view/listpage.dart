f8c1af0cf5994a49e7b71d7f2355501ea86f58c9
AnndreLuiz <andre.aqrt@gmail.com>
2022-07-28 11:53:39 -0300

    testando

---
+++ lib/view/listpage.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:owl_app/view/newassignment.dart';
+
+class ListPage extends StatefulWidget {
+  const ListPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListPage> createState() => _ListPageState();
+}
+
+TextEditingController _Controller = TextEditingController();
+
+class _ListPageState extends State<ListPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(139, 2, 211, 1),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 8, bottom: 10),
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(100),
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: 70,
+                        height: 70,
+                        child: Image.asset(
+                          'lib/images/owl.png',
+                          height: 50,
+                          width: 50,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 60,
+                  ),
+                  Text(
+                    'Suas listagens',
+                    style: TextStyle(
+                      fontFamily: "Montserrat-SemiBold",
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              list2(),
+              SizedBox(
+                height: 10,
+              ),
+              _itemlist(Color.fromRGBO(255, 242, 204, 1), 'Limpar a casa',
+                  'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore ‘erat volutpat. Ut wisi enim ad minim veniam, quis nostrud etation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat'),
+              SizedBox(
+                height: 10,
+              ),
+              _itemlist(Color.fromRGBO(199, 255, 203, 1), 'Pintar a cadeira',
+                  'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore ‘erat volutpat. Ut wisi enim ad minim veniam, quis nostrud etation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat'),
+              SizedBox(
+                height: 10,
+              ),
+              _itemlist(Color.fromRGBO(232, 197, 255, 1), 'Fazer um lanche',
+                  'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore ‘erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.'),
+              SizedBox(
+                height: 10,
+              ),
+              Center(
+                child: Image.asset(
+                  'lib/images/iconadd.png',
+                  height: 80,
+                  width: 80,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget list2() {
+    return Container(
+      margin: EdgeInsets.only(left: 30, right: 30),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
+        ),
+      ),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Busque palavras-chave',
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white, width: 0.0),
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+          suffixIcon: Icon(
+            Icons.search,
+            color: Color(0xFF3101B9),
+          ),
+          labelStyle: TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.w600,
+              color: Color(0xFF3101B9),
+              fontFamily: "tahoma"),
+        ),
+      ),
+    );
+  }
+
+  Widget _itemlist(cor, titulo, descricao) {
+    return Container(
+      height: 120,
+      width: 450,
+      margin: const EdgeInsets.only(left: 30, right: 30),
+      decoration: BoxDecoration(
+        color: cor,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
+        ),
+      ),
+      child: InkWell(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Limpar a casa',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF3101B9),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Text(
+                    'Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore ‘erat volutpat. Ut wisi enim ad minim veniam, quis nostrud etation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat',
+                    style: TextStyle(
+                      color: Color(0xFF3101B9),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Image.asset(
+              'lib/images/iconlixo.png',
+              height: 40,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/newassignment.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/material.dart';
+
+class NewAssignment extends StatefulWidget {
+  const NewAssignment({Key? key}) : super(key: key);
+
+  @override
+  State<NewAssignment> createState() => _NewAssignmentState();
+}
+
+String cor = '$Colors';
+
+class _NewAssignmentState extends State<NewAssignment> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(139, 2, 211, 1),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.only(left: 8, bottom: 10),
+                    width: 100,
+                    height: 100,
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(100),
+                      ),
+                    ),
+                    child: Align(
+                      alignment: Alignment.centerLeft,
+                      child: Container(
+                        width: 70,
+                        height: 70,
+                        child: Image.asset(
+                          'lib/images/owl.png',
+                          height: 50,
+                          width: 50,
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 60,
+                  ),
+                  Text(
+                    'Nova Tarefa',
+                    style: TextStyle(
+                      fontFamily: "Montserrat-SemiBold",
+                      fontSize: 25,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              tituloTarefa(),
+              SizedBox(
+                height: 10,
+              ),
+              descricaoTarefa(),
+              SizedBox(
+                height: 30,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                child: SizedBox(
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      escolherCor(Color.fromRGBO(255, 242, 204, 1)),
+                      escolherCor(Color.fromRGBO(255, 217, 240, 1)),
+                      escolherCor(Color.fromRGBO(232, 197, 255, 1)),
+                      escolherCor(Color.fromRGBO(202, 251, 255, 1)),
+                      escolherCor(Color.fromRGBO(227, 255, 230, 1)),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    'lib/images/iconexcluir.png',
+                    height: 70,
+                  ),
+                  SizedBox(
+                    width: 130,
+                  ),
+                  Image.asset(
+                    'lib/images/iconchecked.png',
+                    height: 70,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget tituloTarefa() {
+    return Container(
+      margin: EdgeInsets.only(
+        left: 30,
+        right: 30,
+      ),
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.all(
+            Radius.circular(10.0),
+          )),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Título da Tarefa',
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white, width: 0.0),
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget descricaoTarefa() {
+    return Container(
+      margin: EdgeInsets.only(
+        left: 30,
+        right: 30,
+      ),
+      height: 210,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(
+          Radius.circular(10.0),
+        ),
+      ),
+      child: TextField(
+        expands: true,
+        maxLines: null,
+        textAlignVertical: TextAlignVertical.top,
+        decoration: InputDecoration(
+          hintText: 'Escreva uma descrição para sua tarefa.',
+          border: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.white, width: 0.0),
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget escolherCor(cor) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        shape: CircleBorder(),
+        primary: cor,
+      ),
+      child: SizedBox(
+        height: 50,
+      ),
+    );
+  }
+}
